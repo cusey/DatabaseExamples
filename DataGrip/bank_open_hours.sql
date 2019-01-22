1ee2097c62ec4aac3a8b4410a89e7764fb734e5a
joe99peace <jjcusey@gmail.com>
2019-01-21 19:58:19 -0500

    Work On Model

---
+++ DataGrip/bank_open_hours.sql
@@ -0,0 +1,6 @@
+INSERT INTO bank.open_hours (open_hours_id, open_hours_name, open_time, close_time, day_of_week) VALUES ('001', 'STANDARD', '9:00 AM', '5:00 PM', 'Monday');
+INSERT INTO bank.open_hours (open_hours_id, open_hours_name, open_time, close_time, day_of_week) VALUES ('002', 'STANDARD', '9:00 AM', '5:00 PM', 'Tuesday');
+INSERT INTO bank.open_hours (open_hours_id, open_hours_name, open_time, close_time, day_of_week) VALUES ('003', 'STANDARD', '9:00 AM', '5:00 PM', 'Wedneday');
+INSERT INTO bank.open_hours (open_hours_id, open_hours_name, open_time, close_time, day_of_week) VALUES ('004', 'STANDARD', '9:00 AM', '5:00 PM', 'Thursday');
+INSERT INTO bank.open_hours (open_hours_id, open_hours_name, open_time, close_time, day_of_week) VALUES ('005', 'STANDARD', '9:00 AM', '5:00 PM', 'Friday');
+INSERT INTO bank.open_hours (open_hours_id, open_hours_name, open_time, close_time, day_of_week) VALUES ('006', 'STANDARD', '11:00 AM', '3:00 PM', 'Saturday');
