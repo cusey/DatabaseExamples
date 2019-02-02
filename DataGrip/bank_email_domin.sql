1b6157b4f74123c946ff52eec9a1b4b7cdb6dd0d
joe99peace <jjcusey@gmail.com>
2019-02-02 11:11:09 -0500

    Delete bank_email_domin.sql

--- DataGrip/bank_email_domin.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO bank.email_domin (email_domain_id, domain) VALUES ('001', 'jnb.com');
-INSERT INTO bank.email_domin (email_domain_id, domain) VALUES ('002', 'gmail.com');
-INSERT INTO bank.email_domin (email_domain_id, domain) VALUES ('003', 'hotmail.com');
-INSERT INTO bank.email_domin (email_domain_id, domain) VALUES ('004', 'yahoo.com');
-INSERT INTO bank.email_domin (email_domain_id, domain) VALUES ('005', 'outlook.com');
-INSERT INTO bank.email_domin (email_domain_id, domain) VALUES ('006', 'mail.com');
